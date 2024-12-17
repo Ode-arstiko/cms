47c69b06193d7068de1c669e8f404a4e01ae97a0
Ode Arstiko <dewiharjanto2@gmail.com>
2024-12-17 20:39:59 +0700

    Add files via upload

---
+++ cms_ode.sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Okt 2024 pada 05.36
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cms_ode`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `caraousel`
+--
+
+CREATE TABLE `caraousel` (
+  `id_caraousel` int(11) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(2, 'Fashion'),
+(3, 'Kendaraan'),
+(4, 'Alat');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konfigurasi`
+--
+
+CREATE TABLE `konfigurasi` (
+  `id_konfigurasi` int(11) NOT NULL,
+  `judul_website` varchar(200) NOT NULL,
+  `profil_website` text NOT NULL,
+  `instagram` varchar(100) NOT NULL,
+  `facebook` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `no_wa` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konten`
+--
+
+CREATE TABLE `konten` (
+  `id_konten` int(11) NOT NULL,
+  `judul` varchar(200) NOT NULL,
+  `keterangan` text NOT NULL,
+  `foto` varchar(50) NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `id_kategori` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `username` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `konten`
+--
+
+INSERT INTO `konten` (`id_konten`, `judul`, `keterangan`, `foto`, `slug`, `id_kategori`, `tanggal`, `username`) VALUES
+(8, 'baju', 'baju bagus', '20241011134459.jpg', 'baju', ' 2 ', '2024-10-11', 'Ode'),
+(11, 'celana', 'popp', '20241011160448.jpg', 'celana', ' 2 ', '2024-10-11', 'Ode'),
+(12, 'bor', 'joss', '20241011160459.jpg', 'bor', ' 4 ', '2024-10-11', 'Ode');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(70) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `level` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `nama`, `password`, `level`) VALUES
+(2, 'Ode', 'Ode Arstiko', '1b979215d058424cd6f7ff99d2e170c8', 'Admin'),
+(3, 'Gugge', 'Gugge', '98e392999f9942d7bc25e6d6accf1c3c', 'Admin'),
+(5, 'Althaf', 'Althaf', 'b9340efbcd5c7b1986f71502f4431098', 'Admin'),
+(6, 'Toko', 'Toko', 'a5629553def252235ffcb85cc7d667e7', 'Admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `caraousel`
+--
+ALTER TABLE `caraousel`
+  ADD PRIMARY KEY (`id_caraousel`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `konfigurasi`
+--
+ALTER TABLE `konfigurasi`
+  ADD PRIMARY KEY (`id_konfigurasi`);
+
+--
+-- Indeks untuk tabel `konten`
+--
+ALTER TABLE `konten`
+  ADD PRIMARY KEY (`id_konten`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `caraousel`
+--
+ALTER TABLE `caraousel`
+  MODIFY `id_caraousel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `konfigurasi`
+--
+ALTER TABLE `konfigurasi`
+  MODIFY `id_konfigurasi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `konten`
+--
+ALTER TABLE `konten`
+  MODIFY `id_konten` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
